07bc85d439a4901eb5aa2e2feb63a3ef4cb2ba7e
Daniel Ochigbo <ochigbodaniel240@gmail.com>
2026-07-07 18:37:45 +0100

    solve: looping a triangle

---
+++ loopingTriangle.js
@@ -0,0 +1,19 @@
+/**
+ * Write a loop that makes seven calls to console.log to output the following triangle:
+ * 
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+let i = 0;
+let e = ""
+
+while(i < 7){
+    console.log(e += "#")
+    i++;
+}
